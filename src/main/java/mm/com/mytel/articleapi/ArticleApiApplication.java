6024cbcec3ae5d3e53fcae5dc0f74307f413378a
vuanh6187 <vubui61200@gmail.com>
2026-05-22 10:01:44 +0700

    Init Project

---
+++ src/main/java/mm/com/mytel/articleapi/ArticleApiApplication.java
@@ -0,0 +1,13 @@
+package mm.com.mytel.articleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mm/com/mytel/articleapi/ArticleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mm.com.mytel.articleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
